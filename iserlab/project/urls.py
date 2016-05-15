d99d27ef54adff1ccb2ca3cba4f6612c2c39a091
mcy <machenyi2011@163.com>
2016-05-16 00:00:54 +0800

    add Test OpenStack Conn-part1

--- iserlab/project/urls.py
+++ iserlab/project/urls.py
@@ -2,9 +2,15 @@ from django.conf.urls import url
 from django.conf.urls import include
 from django.views.generic import ListView,DetailView
 from project.models import Project_info
+from . import views
 
 urlpatterns = [
                url(r'^project/$', ListView.as_view(queryset = Project_info.objects.all().order_by("-createdate")[:25],
                                            template_name = "project/project.html")),
                url(r'^project/(?P<pk>\d+)$', DetailView.as_view(model = Project_info, template_name = 'project/project_detail.html')),
+               url(r'^project/testconn/$',views.testconn, name='testconn'),
+               url(r'^project/testconn/listvm/$',views.listvm, name='listvm'),
+               url(r'^project/testconn/listimage/$',views.listimage, name='listimage'),
+               url(r'^project/testconn/createvm/$',views.createvm, name='createvm'),
+               
 ]
--- iserlab/project/views.py
+++ iserlab/project/views.py
@@ -4,5 +4,16 @@ from django.http import HttpResponse
 from project.models import *
 
 # Create your views here.
-def addProject(request):
-    project_item = 
+
+def testconn(request):
+    return render(request,'testconn/index.html')
+
+def listvm(request):
+    return render(request,'testconn/listvm.html')
+
+def listimage(request):
+    return render(request,'testconn/listimage.html')
+
+def createvm(request):
+    return render(request,'testconn/createvm.html')
+
